7c47e735aedf5f6175f5b375a71de50413d8b60d
CrazyDr <themathprofessor@gmail.com>
2026-08-27 14:44:33 +0300

    Move inactive national account

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:window_manager/window_manager.dart';
 
-const appVersion = 'v0.1.16';
+const appVersion = 'v0.1.17';
 const seedExportDate = '2026-08-27 14:24';
 
 Future<void> main() async {
@@ -155,6 +155,7 @@ AccountType classifyAccount(String account) {
       name.contains('highlow') ||
       name.contains('neteller') ||
       name == '3. εμπορική' ||
+      name == 'εθνική' ||
       name == 'σπίτι 2' ||
       name == 'tdm900') {
     return AccountType.inactive;
